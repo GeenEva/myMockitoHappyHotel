cdb50ff9b6c88bd882c2ae10ab5d96ed40dc810e
Eva van Geenen <evavangeenen@gmail.com>
2021-02-25 13:37:28 +0100

    second commit

---
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test01FirstMocks.java
@@ -0,0 +1,13 @@
+package com.mockitotutorial.happyhotel.booking;
+
+
+
+public class Test01FirstMocks {
+
+    private BookingService bookingService;
+    private PaymentService paymentService;
+    private RoomService roomService;
+    private BookingDAO bookingDAO;
+    private MailSender mailSender;
+
+}
